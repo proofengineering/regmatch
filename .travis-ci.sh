453904cdf9c29e1e7392cb289a1c517fb1a04e62
Karl Palmskog <palmskog@gmail.com>
2017-11-30 17:42:11 -0600

    travis configuration

---
+++ .travis-ci.sh
@@ -0,0 +1,11 @@
+set -ev
+
+opam init --yes --no-setup
+eval $(opam config env)
+
+opam repo add coq-released https://coq.inria.fr/opam/released
+opam repo add proofengineering-dev http://opam-dev.proofengineering.org
+
+opam pin add coq $COQ_VERSION --yes --verbose
+
+opam pin add regexp . --yes --verbose
